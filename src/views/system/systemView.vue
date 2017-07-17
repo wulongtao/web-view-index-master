f158f90d1631b6fa2fd9fa333620ace0cf13a65e
wulongtao <769418278@qq.com>
2017-07-17 23:17:38 +0800

    vue

---
+++ src/views/system/systemView.vue
@@ -0,0 +1,24 @@
+<template lang="html">
+  <div>
+    <h1>system view  system</h1>
+    <input type="text">
+  </div>
+</template>
+
+<script>
+  export default {
+    created() {
+
+    },
+    data() {
+      return {}
+    },
+    computed: {},
+    methods: {},
+    components: {
+    }
+  }
+</script>
+
+<style lang="css">
+</style>
---
+++ src/views/user/user1View.vue
@@ -0,0 +1,45 @@
+<template lang="html">
+  <div>
+    <h1>user view111</h1>
+    <el-input v-model="inputData" placeholder="请输入内容"></el-input>
+  </div>
+</template>
+
+<script>
+  export default {
+    created() {
+      console.log("created")
+    },
+    mounted: function () {
+      console.log("mouted")
+    },
+    activated: function () {
+      console.log("activated")
+      this.initData();
+    },
+    deactivated: function () {
+      console.log("deactivated")
+
+    },
+    data() {
+      return {
+        inputData: 'eee'
+      }
+    },
+    computed: {},
+    methods: {
+      initData: function () {
+        let _this = this;
+        _this.fromLocation = JSON.parse(this.$route.query.fromLocation);
+        _this.toLocation = JSON.parse(this.$route.query.toLocation);
+        _this.activeIndex = parseInt(this.$route.params.activeIndex) || 0;
+        _this.policyType = parseInt(this.$route.params.policyType) || 0;
+      },
+    },
+    components: {
+    }
+  }
+</script>
+
+<style lang="css">
+</style>
